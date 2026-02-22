50cdb7b7937c70bc7ed25bc90859a3934c0149a5
tchulii <Taras.Chulii@gmail.com>
2026-02-22 21:53:13 +0200

    DevOpsII-BuildTools

---
+++ 02-Build-Tools-and-Configuration-Tools/build-tools-TarasChulii/ui/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode : 'development',
+  entry : './src/index.js',
+  output : {
+    filename : 'app.js',
+    path : path.resolve(__dirname, 'dist'),
+  },
+  plugins : [
+    new HtmlWebpackPlugin({
+      template : './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/style.css', to: 'css' },
+      ],
+    }),
+  ],
+};
