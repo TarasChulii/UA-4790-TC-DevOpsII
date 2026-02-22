9030df1987d30b1a410423f6f52ffb5afd1b66a2
tchulii <Taras.Chulii@gmail.com>
2026-02-22 22:04:10 +0200

    Add some comments

--- 02-Build-Tools-and-Configuration-Tools/build-tools-TarasChulii/ui/webpack.config.js
+++ 02-Build-Tools-and-Configuration-Tools/build-tools-TarasChulii/ui/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
+// Prior to use need to install 
+// npm install --save-dev html-webpack-plugin
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+// Prior to use need to install
+// npm install copy-webpack-plugin --save-dev
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
